acb7d22fef1c68d9567d8074d7df963885a99d6f
Fahim Sheikh <fsheikh2@illinois.edu>
2025-07-17 12:59:37 -0500

    Updated to include more beans

--- Ch2/sq-c2-ex2/src/main/java/org/example/config/ProjectConfig.java
+++ Ch2/sq-c2-ex2/src/main/java/org/example/config/ProjectConfig.java
@@ -3,13 +3,42 @@ package org.example.config;
 import org.example.main.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    Parrot parrot(){
+    @Primary
+    Parrot parrot() {
+        var p = new Parrot("Miki");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot1(){
         var p = new Parrot("Koko");
         return p;
+    }
+    @Bean
+    Parrot parrot2(){
+        var p = new Parrot("Kiki");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot3(){
+        var p = new Parrot("Riki");
+        return p;
+    }
+
+    @Bean
+    String hello(){
+        return "hello";
+    }
+
+    @Bean
+    Integer ten(){
+        return 10;
     }
 }
--- Ch2/sq-c2-ex2/src/main/java/org/example/main/Main.java
+++ Ch2/sq-c2-ex2/src/main/java/org/example/main/Main.java
@@ -7,7 +7,22 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot p = context.getBean(Parrot.class);
-        System.out.println(p.getName());
+        Parrot p1 = context.getBean("parrot1", Parrot.class);
+        System.out.println(p1.getName());
+
+        Parrot p2 = context.getBean("parrot2", Parrot.class);
+        System.out.println(p2.getName());
+
+        Parrot p3 = context.getBean("parrot3", Parrot.class);
+        System.out.println(p3.getName());
+
+        Parrot theParrot = context.getBean(Parrot.class);
+        System.out.println(theParrot.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer i = context.getBean(Integer.class);
+        System.out.println(i);
     }
 }
