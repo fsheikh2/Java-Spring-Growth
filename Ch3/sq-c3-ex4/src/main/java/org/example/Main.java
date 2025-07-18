582726ad14cd2b4492831fc06ccdd594bdd686b2
fahimsheikh <fsheikh2@illinois.edu>
2025-07-18 17:00:01 -0500

    Completed the example for autowiring using stereotype annoations.

--- Ch3/sq-c3-ex4/src/main/java/org/example/Main.java
+++ Ch3/sq-c3-ex4/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import config.ProjectConfig;
+import main.Person;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/*
+* The purpose of this example is to show how to autowire a bean while using stereotype annotations.
+* We've marked Person and Parrot as components for the spring context and used @Autowired on the Parrot
+* field in the Person class. This tells Spring to provide the Person bean with a Parrot bean from the
+* context.
+* */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person p = context.getBean(Person.class);
+
+        System.out.println("Person's name: " + p.getName());
+        System.out.println("Person's parrot: " + p.getParrot());
     }
 }
