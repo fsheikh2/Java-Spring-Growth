74de7e4ace41615504bf0944d1bdc03e94ce98ea
fahimsheikh <fsheikh2@illinois.edu>
2025-07-19 22:35:59 -0500

    Finsihed example 8.

---
+++ Ch3/sq-c3-ex8/src/main/java/beans/Person.java
@@ -0,0 +1,20 @@
+package beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name = "Ella";
+    private final Parrot parrot;
+
+    public Person(Parrot parrot2) {
+        this.parrot = parrot2;
+    }
+
+    public String getName(){ return name; }
+
+    public Parrot getParrot(){ return parrot; }
+
+    public void setName(String name) { this.name = name; }
+}
--- Ch3/sq-c3-ex8/src/main/java/org/example/Main.java
+++ Ch3/sq-c3-ex8/src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import beans.Person;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/*
+* The purpose of this example is to show another way to choose between multiple beans that is a dependency
+* for another bean. Similar to the difference between examples 6 and 7, instead of using the name of the
+* bean as the parameter in the constructor, we can use the @Qualifier annotation instead.
+* */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person p = context.getBean(Person.class);
+        System.out.println("Person name: " + p.getName());
+        System.out.println("Parrot name: " + p.getParrot());
     }
 }
