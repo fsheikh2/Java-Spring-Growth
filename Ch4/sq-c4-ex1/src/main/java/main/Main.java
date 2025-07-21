1e2178fbad3a7cdd04fb9abd2c3d089681b28e1f
fahimsheikh <fsheikh2@illinois.edu>
2025-07-21 16:34:57 -0500

    added comment explaining example 1's purpose

--- Ch4/sq-c4-ex1/src/main/java/main/Main.java
+++ Ch4/sq-c4-ex1/src/main/java/main/Main.java
@@ -2,9 +2,13 @@ package main;
 
 import model.Comment;
 import proxies.EmailNotificationProxy;
-import repositories.CommentRepository;
 import repositories.DBCommentRepository;
 import services.CommentService;
+
+/*
+* The purpose of this exercise is to show a demo of an app which posts users comments and sends an
+* email notification to the commenter. This is done without the use of Spring.
+* */
 
 public class Main {
     public static void main(String[] args) {
