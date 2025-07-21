f21f9e7f7a1391472a869c296c2c040f11e170a5
fahimsheikh <fsheikh2@illinois.edu>
2025-07-21 16:24:25 -0500

    Added the configuration annotation for the project configuration class.

--- Ch4/sq-c4-ex2/src/main/java/configs/ProjectConfiguration.java
+++ Ch4/sq-c4-ex2/src/main/java/configs/ProjectConfiguration.java
@@ -1,7 +1,9 @@
 package configs;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ComponentScan(basePackages = {"proxies", "repositories", "services"})
 public class ProjectConfiguration {
 }
