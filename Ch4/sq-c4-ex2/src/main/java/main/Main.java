94ec8d17dbb058867ea691b3c2e965800571ce05
fahimsheikh <fsheikh2@illinois.edu>
2025-07-21 16:37:20 -0500

    Added a comment explaining the purpose of this exercise.

--- Ch4/sq-c4-ex2/src/main/java/main/Main.java
+++ Ch4/sq-c4-ex2/src/main/java/main/Main.java
@@ -5,6 +5,12 @@ import model.Comment;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
 
+/*
+* The purpose of this example is to demo the same comment posting and notification app as in the first
+* exmaple but with the use of Spring. Specifically we use sterotype annotations and autowiring to set up
+* the CommentService object.
+* */
+
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
