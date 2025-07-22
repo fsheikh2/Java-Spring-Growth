dcb605344901be116959f94267917df5d78bc742
fahimsheikh <fsheikh2@illinois.edu>
2025-07-22 00:31:49 -0500

    Finished example 6.

--- Ch4/sq-c4-ex6/src/main/java/services/CommentService.java
+++ Ch4/sq-c4-ex6/src/main/java/services/CommentService.java
@@ -2,6 +2,7 @@ package services;
 
 import model.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
@@ -14,7 +15,7 @@ public class CommentService {
 
     @Autowired
     public CommentService(CommentRepository commentRepository,
-                          CommentNotificationProxy commentNotificationProxy) {
+                          @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
