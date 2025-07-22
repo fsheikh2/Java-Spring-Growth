5d92ffa240722a844542c8646c9327bbe35a6944
fahimsheikh <fsheikh2@illinois.edu>
2025-07-22 00:17:25 -0500

    Finished example 5

--- Ch4/sq-c4-ex5/src/main/java/main/Main.java
+++ Ch4/sq-c4-ex5/src/main/java/main/Main.java
@@ -5,6 +5,11 @@ import model.Comment;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
 
+/*
+* This example continues developing the same app but shows one way to handle the situation when we have
+* more than one bean which fulfills a "contract", i.e. implements an interface, in the spring context.
+* Specifically we mark the bean we want to use as @Primary.
+* */
 public class Main {
     public static void main(String[] args) {
 
