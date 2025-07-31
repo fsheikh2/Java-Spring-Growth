5dd98305478c68132a18fff4627e565a853095b7
fahimsheikh <fsheikh2@illinois.edu>
2025-07-31 11:51:26 -0500

    Adding Ch7 directory and example 1

---
+++ Ch7/sq-c7-ex1/src/main/java/org/example/sqc7ex1/Main.java
@@ -0,0 +1,13 @@
+package org.example.sqc7ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ Ch7/sq-c7-ex1/src/test/java/org/example/sqc7ex1/SqC7Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqc7ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqC7Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
