659fad07bbc8f9c74fa2047d49af06b8bdc62d52
fahimsheikh <fsheikh2@illinois.edu>
2025-08-05 13:18:35 -0500

    Adding chapter 8 directory and unfinished example 1 project.

---
+++ Ch8/sq-c8-ex1/src/main/java/org/example/sqc8ex1/SqC8Ex1Application.java
@@ -0,0 +1,13 @@
+package org.example.sqc8ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqC8Ex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqC8Ex1Application.class, args);
+    }
+
+}
---
+++ Ch8/sq-c8-ex1/src/test/java/org/example/sqc8ex1/SqC8Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqc8ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqC8Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
