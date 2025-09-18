d731918fc75d7107895b60b2171b17725a0c7e3d
fahimsheikh <fsheikh2@illinois.edu>
2025-09-17 22:50:13 -0500

    Pushing Ch8

---
+++ Ch8/sq-c8-ex3/src/main/java/com/example/SqC8Ex3Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqC8Ex3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqC8Ex3Application.class, args);
+    }
+
+}
