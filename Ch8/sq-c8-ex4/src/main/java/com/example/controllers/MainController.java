12cfc712e9d620f65bb50ed0318d755626675620
fahimsheikh <fsheikh2@illinois.edu>
2025-09-17 23:18:26 -0500

    adding exapmle 4

--- Ch8/sq-c8-ex4/src/main/java/com/example/controllers/MainController.java
+++ Ch8/sq-c8-ex4/src/main/java/com/example/controllers/MainController.java
@@ -1,4 +1,17 @@
 package com.example.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+
+    @RequestMapping("/home/{username}/{color}")
+    public String home(@PathVariable String color, @PathVariable String username, Model model) {
+        model.addAttribute("color", color);
+        model.addAttribute("username", username);
+        return "home.html";
+    }
 }
