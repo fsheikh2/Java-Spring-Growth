0e9d924ff4a4c1ee4e9a9858e5e1923133dff305
fahimsheikh <fsheikh2@illinois.edu>
2025-09-17 23:10:26 -0500

    Adding example 4

---
+++ Ch8/sq-c8-ex4/src/main/java/com/example/SqC8Ex4Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqC8Ex4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqC8Ex4Application.class, args);
+    }
+
+}
